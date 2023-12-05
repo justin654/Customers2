5b13a0d75a5e7719501f65d8324643d5a1e6ae35
justin654 <justin654@gmail.com>
2023-12-04 22:48:23 -0500

    Update Customers2.java
    Commented

--- Customers2.java
+++ Customers2.java
@@ -10,86 +10,88 @@
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class Customers2 {
-    private JFrame frame;
-    private JTextField accNumField, beginBalField, totalNewField, totalPaidField, creditLimitField;
-    private JTextArea resultArea;
+    private JFrame frame; // JFrame for the main window
+    private JTextField[] textFields; // Array to store text fields for user input
+    private JTextArea resultArea; // Text area for displaying the result
 
     public Customers2() {
-        initialize();
+        initialize(); // Initialize the GUI components
     }
 
     private void initialize() {
-        frame = new JFrame("Credit Card Calculator");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new GridBagLayout());
-        GridBagConstraints gbc = new GridBagConstraints();
+        frame = new JFrame("Credit Card Calculator"); // Create the main frame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Set close operation
+        frame.setLayout(new GridBagLayout()); // Use GridBagLayout for layout
+        GridBagConstraints gbc = new GridBagConstraints(); // GridBagConstraints for layout control
+        gbc.insets = new Insets(4, 4, 4, 4); // Padding between components
+        int rowIndex = 0; // Current row index for grid layout
 
-        // Setting common constraints for labels and fields
-        gbc.insets = new Insets(4, 4, 4, 4);
-        gbc.anchor = GridBagConstraints.EAST;
-        int rowIndex = 0;
+        textFields = new JTextField[5]; // Initialize an array to store text fields
 
-        accNumField = addLabelAndTextField("Account Number:", rowIndex++, 10);
-        beginBalField = addLabelAndTextField("Beginning Balance:", rowIndex++, 10);
-        totalNewField = addLabelAndTextField("New Charges:", rowIndex++, 10);
-        totalPaidField = addLabelAndTextField("Total Paid:", rowIndex++, 10);
-        creditLimitField = addLabelAndTextField("Credit Limit:", rowIndex++, 10);
+        // Labels for user input fields
+        String[] labels = {"Account Number:", "Beginning Balance:", "New Charges:", "Total Paid:", "Credit Limit:"};
 
+        // Create and add labels and text fields for user input
+        for (int i = 0; i < labels.length; i++) {
+            textFields[i] = addLabelAndTextField(labels[i], rowIndex++); // Add label and text field
+        }
+
+        // Create and add a "Calculate" button with an ActionListener
         JButton calculateButton = new JButton("Calculate");
-        calculateButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                calculateResult();
-            }
-        });
-        gbc.gridx = 1;
-        gbc.gridy = rowIndex++;
-        frame.add(calculateButton, gbc);
+        calculateButton.addActionListener(e -> calculateResult()); // Define the action when the button is clicked
+        gbc.gridx = 1; // Set grid X position
+        gbc.gridy = rowIndex++; // Increase row index
+        frame.add(calculateButton, gbc); // Add the button to the frame
 
-        resultArea = new JTextArea(5, 30);
-        resultArea.setEditable(false);
-        gbc.gridx = 0;
-        gbc.gridy = rowIndex;
-        gbc.gridwidth = 2;
-        frame.add(new JScrollPane(resultArea), gbc);
+        // Create and add a text area for displaying the result (with scroll pane)
+        resultArea = new JTextArea(5, 30); // Rows and columns for text area
+        resultArea.setEditable(false); // Make it non-editable
+        gbc.gridx = 0; // Reset grid X position
+        gbc.gridy = rowIndex; // Set the grid row
+        gbc.gridwidth = 2; // Span across two columns
+        frame.add(new JScrollPane(resultArea), gbc); // Add the scrollable text area to the frame
 
-        frame.pack();
-        frame.setLocationRelativeTo(null); // Center the frame
-        frame.setVisible(true);
+        frame.pack(); // Pack the components for optimal size
+        frame.setLocationRelativeTo(null); // Center the frame on the screen
+        frame.setVisible(true); // Make the frame visible
     }
 
-    private JTextField addLabelAndTextField(String label, int row, int width) {
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.insets = new Insets(4, 4, 4, 4);
-        gbc.anchor = GridBagConstraints.EAST;
-        gbc.gridx = 0;
-        gbc.gridy = row;
-        frame.add(new JLabel(label), gbc);
+    // Method to add a label and text field to the frame
+    private JTextField addLabelAndTextField(String label, int row) {
+        GridBagConstraints gbc = new GridBagConstraints(); // GridBagConstraints for layout control
+        gbc.insets = new Insets(4, 4, 4, 4); // Padding between components
+        gbc.anchor = GridBagConstraints.EAST; // Align components to the right
+        gbc.gridx = 0; // Label in column 0
+        gbc.gridy = row; // Specified row for the label
 
-        JTextField textField = new JTextField(width);
-        gbc.gridx = 1;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        frame.add(textField, gbc);
+        frame.add(new JLabel(label), gbc); // Create and add a label
 
-        return textField;
+        JTextField textField = new JTextField(10); // Create a text field with columns
+        gbc.gridx = 1; // Text field in column 1
+        gbc.fill = GridBagConstraints.HORIZONTAL; // Allow horizontal expansion
+        frame.add(textField, gbc); // Add the text field
+
+        return textField; // Return the created text field
     }
 
+    // Method to calculate and display the result
     private void calculateResult() {
         try {
-            int accNum = Integer.parseInt(accNumField.getText());
-            double beginBal = Double.parseDouble(beginBalField.getText());
-            double totalNew = Double.parseDouble(totalNewField.getText());
-            double totalPaid = Double.parseDouble(totalPaidField.getText());
-            double creditLimit = Double.parseDouble(creditLimitField.getText());
+            // Parse input values from text fields
+            int accNum = Integer.parseInt(textFields[0].getText());
+            double beginBal = Double.parseDouble(textFields[1].getText());
+            double totalNew = Double.parseDouble(textFields[2].getText());
+            double totalPaid = Double.parseDouble(textFields[3].getText());
+            double creditLimit = Double.parseDouble(textFields[4].getText());
 
+            // Calculate various values
             double owedBal = beginBal + totalNew - totalPaid;
             double overLimitBy = owedBal - creditLimit;
             double availCredit = creditLimit - owedBal;
 
+            // Prepare the result message
             StringBuilder result = new StringBuilder();
             result.append("Account Number: ").append(accNum).append("\n");
             result.append(String.format("Ending Balance: $%.2f \n", owedBal));
@@ -102,14 +104,13 @@ public class Customers2 {
                 result.append("Warning: You are at your credit limit.");
             }
 
-            resultArea.setText(result.toString());
+            resultArea.setText(result.toString()); // Display the result in the text area
         } catch (NumberFormatException ex) {
             JOptionPane.showMessageDialog(frame, "Invalid input. Please enter correct values.", "Input Error", JOptionPane.ERROR_MESSAGE);
         }
-
     }
 
     public static void main(String[] args) {
-        new Customers2();
+        new Customers2(); // Create an instance of the Customers2 class
     }
 }
